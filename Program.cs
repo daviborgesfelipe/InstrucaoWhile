b4fd2111ca739749769992c1252f85ffb72dc764
Davi Borges <davifelipe_300@hotmail.com>
2022-02-03 16:09:54 -0300

    adicionando arquivos

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace InstrucaoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = 0;
+
+            while(contador <= 10)
+            {
+                Console.WriteLine(contador);
+                contador = contador + 1;        
+            }
+        }
+    }
+}
